f5ad82b4d1098c318f410edd26bf9c0bd07d707f
zbexworld <zbexworld@gmail.com>
2021-01-26 01:03:11 +0100

    commit for heroku

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const express = require('express');
+const app= express();
+const port = process.env.PORT || 3000;
+
+const publicPath = path.resolve(__dirname, '..', 'public');
+
+
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(publicPath, 'index.html'));
+})
+
+app.listen(port, () => {
+    console.log('server is running');
+})
